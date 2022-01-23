85249d0e52f63709042d1a4d027c103389d7deb4
OlmioGIT <olmihailoveg@gmail.com>
2022-01-23 15:18:25 +0300

    good joob

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,65 +12,42 @@ const val VKPAY = "VK Pay"
 
 fun main() {
     val amount: Double = 8000015.0
-    val weeklyAmount = 15768870
+    val weeklyAmount = 6000000
     val card = "Maestro"
-    checkInfo(amount, card, weeklyAmount)
+    goCal(amount, card, weeklyAmount)
 }
 
-fun checkInfo(amount: Double, card: String = VKPAY, weeklyAmount: Int = 0){
+fun goCal(amount: Double, card: String = VKPAY, weeklyAmount: Int = 0){
     when (card) {
-        VKPAY->{calculateVK(amount,weeklyAmount)}
-        MIR->{calculateVM(amount,weeklyAmount)}
-        VISA->{calculateVM(amount, weeklyAmount)}
-        MAESTRO->{calculateMCM(amount,weeklyAmount)}
-        MC->{calculateMCM(amount,weeklyAmount)}
+        VKPAY-> { if (weeklyAmount > 4000000 || amount > 1500000) {print("Нельзя перевести более 40000 рублей в месяц и более 15000 рублей за раз.")}
+        else   printAll(calculateAll(amount,card))}
+        MIR->{if (weeklyAmount > 60000000 || amount > 15000000){print("Нельзя перевести более 600000 рублей в месяц и более 150000 рублей за раз.")}
+        else printAll(calculateAll(amount,card))}
+        VISA->{if (weeklyAmount > 60000000 || amount > 15000000){print("Нельзя перевести более 600000 рублей в месяц и более 150000 рублей за раз.")}
+        else  printAll(calculateAll(amount,card))}
+        MAESTRO->{if (weeklyAmount > 60000000 || amount > 15000000){print("Нельзя перевести более 600000 рублей в месяц и более 150000 рублей за раз.")}
+        else  printAll(calculateAll(amount,card))}
+        MC->{if (weeklyAmount > 60000000 || amount > 15000000){print("Нельзя перевести более 600000 рублей в месяц и более 150000 рублей за раз.")}
+        else  printAll(calculateAll(amount,card))}
     }
 }
 
-fun calculateVM(amount: Double, weeklyAmount: Int): Double {
+fun calculateAll(amount: Double, card: String = VKPAY): Double {0
+    val comm =  amount * 0.0075
+    return when (card) {
+        MAESTRO, MC -> if (amount in 30001.0..7499999.0) {
+            return amount * 0
+        } else {
+            return ((amount * 0.006) + 2000)
+        }
+        MIR, VISA ->  if (comm < 3500 ) {return 3500.0} else { return comm}
 
-    return if (weeklyAmount > 60000000 && amount > 15000000) {
-        amount
-    } else {
-        amount * 0.0075
+        else -> return amount * 0
     }
 }
 
-fun calculateMCM(amount:Double, weeklyAmount: Int){
-    val commission = (amount * 0.006) + 2000
+fun printAll(commission: Double) {
     val cent = (commission % 100).toInt()
     val rub: Int = (commission / 100) .toInt()
-    if (weeklyAmount>60000000)
-        print("Нельзя перевести более 600000 рублей в месяц.")
-    else if (amount>15000000)
-        print("Нельзя перевести более 150000 рублей за один раз.")
-    else if (amount in 30001..7499999)
-        print("Комиссия составит 0 рублей")
-    else
-        println("Комиссия составит $rub рублей $cent копеек.")
+    println("Комиссия составит $rub рублей $cent копеек.")
 }
-
-fun calculateVK(amount: Double, weeklyAmount: Int){
-
-    if (weeklyAmount>4000000)
-        print("Нельзя перевести более 40000 рублей в месяц.")
-    else if (amount>1500000)
-        print("Нельзя перевести более 15000 рублей за один раз.")
-    else  (amount<30000)
-    print("Комиссия составит 0 рублей")
-}
-
-fun printComVM (amount: Int) {
-    prinln(amount)
-}
-
-fun printComMCM (amount: Int) {
-    prinln(amount)
-}
-
-fun printComVK (amount: Int) {
-    prinln(amount)
-}
-
-//val cent = (commission % 100).toInt()
-//        val rub: Int = (commission / 100) .toInt()
